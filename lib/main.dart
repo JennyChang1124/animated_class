ca0f70668c97e349471152727f14d41d39077414
張茜雯 <112846217+JennyChang1124@users.noreply.github.com>
2022-11-23 18:30:27 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,82 +2,66 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  static const String _title = 'Animation  Sample';
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: GFG(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
+class GFG extends StatefulWidget {
+  const GFG({Key? key}) : super(key: key);
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<GFG> createState() => _GFGState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  double padValue = 500.0;
-  double padValue2 =500.0;
-  void _updatePadding(double value) {
-    setState(() {
-      padValue = value;
-      padValue2 = value;
-    });
-  }
+class _GFGState extends State<GFG> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  bool _bool = true;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        AnimatedPadding(
-            padding: EdgeInsets.only(left:150,top: padValue2),
-            duration: const Duration(seconds: 3),
-            curve: Curves.easeOut,
-            child: const Icon(
-              Icons.airplanemode_active,
-              color: Colors.blue,
-              size: 50,
-            ),
-            onEnd: (){
-              setState(() {
-                padValue2 = 400;
-              });
-            }
-        ),
-        const SizedBox(height: 10),
-        Padding(
-          padding: const EdgeInsets.only(left:150,top: 10),
-          child:  ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                    Colors.blue),),
-                    onPressed: () {
-                _updatePadding(padValue2== 0.0 ? 100.0 : 0.0);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animation  Sample"),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              setState(
+                    () {
+                  _bool = !_bool;
                 },
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Icon(Icons.airplanemode_active_outlined),
-                  Text('起飛'),
-                ],
-              ),
+              );
+            },
+            child: Text("切換"),
           ),
-        ),
-      ],
-    ),
+        ],
+      ),
+      body: Center(
+        child: AnimatedCrossFade(
+          // First widget
+            firstChild: Container(
+              child:Icon(Icons.circle_notifications_outlined,size: 60,),
+            ),
+            secondChild: Container(
+                child:Icon(Icons.ice_skating,size: 60,)
+            ),
+            crossFadeState:
+            _bool ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+            // Duration for crossFade animation.
+            duration: Duration(seconds: 1)),
+      ),
     );
   }
 }
